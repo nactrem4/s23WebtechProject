eab0b29ddcdc38c3393d32b883155fa5ff8e613d
mertc <mert.can58@hotmail.de>
2023-04-24 14:51:34 +0200

    meilenstein 1

---
+++ src/main/java/com/webech/project/demo/web/Controller.java
@@ -0,0 +1,14 @@
+package com.webech.project.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping(path = "/")
+    public ModelAndView helloWorldPage(){
+        return new ModelAndView("helloworld");
+    }
+
+}
